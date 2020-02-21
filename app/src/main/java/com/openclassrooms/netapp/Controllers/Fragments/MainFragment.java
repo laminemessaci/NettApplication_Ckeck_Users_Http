d9d7bad64384a305b03696a43090a064d9eb3d3c
messaci <lamine.messaci18@gmail.com>
2020-02-21 14:53:07 +0100

    Creating  MyHttpURLConnection and NetworkAsyncTask

---
+++ app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/MainFragment.java
@@ -0,0 +1,78 @@
+package com.openclassrooms.netapp.Controllers.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.openclassrooms.netapp.Controllers.utils.NetworkAsyncTask;
+import com.openclassrooms.netapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners {
+
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_textview) TextView textView;
+
+    public MainFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        //textView.findViewById (R.id.fragment_main_textview);
+        return view;
+    }
+
+    // -----------------
+    // ACTIONS
+    // -----------------
+
+    @OnClick(R.id.fragment_main_button)
+    public void submit(View view) {
+        this.executeHttpRequest();
+    }
+
+    // ------------------
+    //  HTTP REQUEST
+    // ------------------
+
+    private void executeHttpRequest(){
+        new NetworkAsyncTask(this).execute("https://api.github.com/users/JakeWharton/following");
+    }
+
+    @Override
+    public void onPreExecute() {
+        this.updateUIWhenStartingHTTPRequest();
+    }
+
+    @Override
+    public void doInBackground() { }
+
+    @Override
+    public void onPostExecute(String json) {
+        this.updateUIWhenStopingHTTPRequest(json);
+    }
+
+    // ------------------
+    //  UPDATE UI
+    // ------------------
+
+    private void updateUIWhenStartingHTTPRequest(){
+        this.textView.setText("Downloading...");
+    }
+
+    private void updateUIWhenStopingHTTPRequest(String response){
+        this.textView.setText(response);
+    }
+
+}
