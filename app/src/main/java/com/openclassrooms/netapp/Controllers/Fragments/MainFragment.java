319672565cfc93b2f870bdfe1dc3a462e377bc05
messaci <lamine.messaci18@gmail.com>
2020-02-22 18:15:55 +0100

    Update MainFragment

--- app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/MainFragment.java
+++ app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/MainFragment.java
@@ -2,14 +2,19 @@ package com.openclassrooms.netapp.Controllers.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.openclassrooms.netapp.Controllers.model.GithubUser;
+import com.openclassrooms.netapp.Controllers.utils.GithubCalls;
 import com.openclassrooms.netapp.Controllers.utils.NetworkAsyncTask;
 import com.openclassrooms.netapp.R;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,7 +23,7 @@ import butterknife.OnClick;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners {
+public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners, GithubCalls.Callbacks {
 
     // FOR DESIGN
     @BindView(R.id.fragment_main_textview) TextView textView;
@@ -39,8 +44,45 @@ public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners
 
     @OnClick(R.id.fragment_main_button)
     public void submit(View view) {
-        this.executeHttpRequest();
+        this.executeHttpRequestWithRetrofit();
     }
+
+    /**
+    // 4 - Execute HTTP request and update UI
+    private void executeHttpRequestWithRetrofit(){
+        this.updateUIWhenStartingHTTPRequest();
+        GithubCalls.fetchUserFollowing(this, "JakeWharton");
+    }
+     */
+    //---------------------------------------------------------------------------//
+
+    // ------------------------------
+    //  HTTP REQUEST (Retrofit Way)
+    // ------------------------------
+
+    // 4 - Execute HTTP request and update UI
+    private void executeHttpRequestWithRetrofit(){
+        this.updateUIWhenStartingHTTPRequest();
+        GithubCalls.fetchUserFollowing(this, "JakeWharton");
+    }
+
+
+    //Overide callBack methods
+
+    @Override
+    public void onResponse (@Nullable List<GithubUser> users) {
+        // 2.1 - When getting response, we update UI
+        if (users != null) this.updateUIWithListOfUsers(users);
+
+    }
+
+    @Override
+    public void onFailure () {
+        // 2.2 - When getting error, we update UI
+        this.updateUIWhenStopingHTTPRequest("An error happened !");
+
+    }
+
 
     // ------------------
     //  HTTP REQUEST
@@ -74,5 +116,17 @@ public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners
     private void updateUIWhenStopingHTTPRequest(String response){
         this.textView.setText(response);
     }
+
+
+    // 3 - Update UI showing only name of users
+
+    private void updateUIWithListOfUsers(List<GithubUser> users){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (GithubUser user : users){
+            stringBuilder.append("-"+user.getLogin()+"\n");
+        }
+        updateUIWhenStopingHTTPRequest(stringBuilder.toString());
+    }
+
 
 }
--- app/src/main/java/com/openclassrooms/netapp/Controllers/utils/GithubService.java
+++ app/src/main/java/com/openclassrooms/netapp/Controllers/utils/GithubService.java
@@ -1,6 +1,24 @@
 package com.openclassrooms.netapp.Controllers.utils;
 
+import com.openclassrooms.netapp.Controllers.model.GithubUser;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface GithubService {
+
+    @GET ("users/{username}/following")
+    Call<List<GithubUser>> getFollowing(@Path ("username") String username);
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
 
 
 }
